aeb5670671145ee435d106a86c6453d9db652bfb
Aviral Srivastava <63746541+Aviral-3@users.noreply.github.com>
2024-02-23 10:41:08 +0530

    Final Update

--- chatBot.py
+++ chatBot.py
@@ -1,4 +1,3 @@
-import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import spacy
@@ -6,6 +5,7 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 
 def fetch_webpage_content(url):
+    """Fetching the webpage content at the specified URL."""
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     text = ' '.join(soup.stripped_strings)
@@ -17,6 +17,7 @@ def process_text_with_spacy(text):
     return sentences
 
 def find_answer_with_spacy(query, sentences):
+    """Finding the answer to the query within the given sentences using SpaCy."""
     query_doc = nlp(query)
     query_keywords = [token.lemma_.lower() for token in query_doc if token.is_stop == False and token.is_punct == False]
     best_match = None
@@ -28,20 +29,18 @@ def find_answer_with_spacy(query, sentences):
         if score > best_score:
             best_score = score
             best_match = sentence
-    return best_match if best_match else "Sorry, I couldn't find an answer to your question."
+    return best_match if best_match else "Sorry, I couldn't find an answer to the question."
 
 def chatbot(url, query):
+    """The chatbot function to get an answer for the query from the webpage at the given URL."""
     content = fetch_webpage_content(url)
     sentences = process_text_with_spacy(content)
     answer = find_answer_with_spacy(query, sentences)
     return answer
 
-st.title('Web Content-based Chatbot')
-
-url = st.text_input('Enter the URL of the webpage:', 'https://www.cranberry.fit/post/why-don-t-i-sleep-well-before-my-period')
-query = st.text_input('Enter your query:', 'What causes period cramps?')
-
-if st.button('Find Answer'):
+# Example usage
+if __name__ == "__main__":
+    url = input("Enter the URL of the webpage: ")
+    query = input("Enter your query: ")
     response = chatbot(url, query)
-    st.text_area("Answer", value=response, height=150, max_chars=None, help=None)
-
+    print("Answer:", response)
